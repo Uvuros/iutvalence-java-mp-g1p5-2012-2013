9b3411b7ae0ee5375aeaae5e41aed0eceae96d50
Uvuros <guillaume.pignet@laposte.net>
2012-10-18 08:07:55 +0200

    Ajout de la classe JoueurClavier

--- src/fr/iutvalence/java/projets/shootemup/JoueurClavier.java
+++ src/fr/iutvalence/java/projets/shootemup/JoueurClavier.java
@@ -1,23 +1,19 @@
 package fr.iutvalence.java.projets.shootemup;
 
-
-// FIXME (FIXED) détailler le commentaire
 /**
  * Classe de controle
  * interface entre l'utilisateur et la partie
  * @author deguitre & Pignet
  * 
  */
-public class JoueurAlea implements Joueur
+public class JoueurClavier implements Joueur
 {
 	/* (non-Javadoc)
 	 * @see fr.iutvalence.java.projets.shootemup.Joueur#getDeplacement()
 	 * @Override
 	 */
-	
 	public int getDeplacement()
 	{
-		return (int)(Math.random() *3); 
+		return 0; 
 	}
-	
 }

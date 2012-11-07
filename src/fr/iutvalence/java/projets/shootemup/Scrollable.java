3e5b6038d7a10960c5548d1650c09aad765b25b7
Uvuros <guillaume.pignet@laposte.net>
2012-11-07 15:28:10 +0100

    Retrait des méthodes inappropriées de Scrollable

--- src/fr/iutvalence/java/projets/shootemup/Scrollable.java
+++ src/fr/iutvalence/java/projets/shootemup/Scrollable.java
@@ -13,15 +13,4 @@ public interface Scrollable
 	 * @return resultat du scroll
 	 */
 	public boolean scroll();
-	/**
-	 * deplacement ( à compléter)
-	 * @param direction direction 
-	 * @return true si vivant mort sinon ( a faire)
-	 */
-	public boolean mouvement(Direction direction);
-	/**
-	 * @return l'état du vaisseau sous forme de booléen 
-	 */
-	public boolean enVie();
-
 }
